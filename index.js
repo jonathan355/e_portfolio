935bb4a8febc7a80fb50b098fb602b696f35e031
jonathan355 <jonathan35587@gmail.com>
2025-07-09 13:03:35 -0500

    created background feature

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+let contrastToggle = false;
+let isModalOpen = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2  !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = 'translate(${x * boolInt}px, ${y * boolInt}px'
+    }
+    
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact (event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
@@ -21,7 +48,7 @@ function contact (event) {
 
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
